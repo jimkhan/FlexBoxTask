60b5159a19c20526dfdfee8defa88b7a2156db5b
Mainframe Labs Ltd <jim.khan88@gmail.com>
2021-12-30 12:06:10 +0600

    initial commit

--- ios/Pods/Target Support Files/Pods-Sohopathi-SohopathiTests/Pods-Sohopathi-SohopathiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sohopathi_SohopathiTests : NSObject
-@end
-@implementation PodsDummy_Pods_Sohopathi_SohopathiTests
-@end
--- ios/Pods/Target Support Files/Pods-Sohopathi/Pods-Sohopathi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sohopathi : NSObject
-@end
-@implementation PodsDummy_Pods_Sohopathi
-@end
